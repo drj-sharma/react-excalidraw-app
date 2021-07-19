86bf32d214f6f65e6bec4e067b950b4d2d0d5fa3
Dheeraj Sharma <dheeraj.sharma04@nagarro.com>
2021-07-19 11:46:20 +0530

    added comments

--- src/excalidraw/apis/getAll-Images.js
+++ src/excalidraw/apis/getAll-Images.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'http://localhost:3333'
 });
+// get all uploaded images' endpoints from the server
 const getAllImages = async () => {
   const data = await api.get('/all-images');
   return data.data;
